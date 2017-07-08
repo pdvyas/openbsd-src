fc95c820771ff818b4cf3616c675cab40ed182b9
Pratik Vyas <m@pd.io>
2017-07-07 21:25:44 -0700

    send / recv split diff

--- usr.sbin/vmd/control.c
+++ usr.sbin/vmd/control.c
@@ -71,7 +71,7 @@ control_run(struct privsep *ps, struct privsep_proc *p, void *arg)
 	 * unix - for the control socket.
 	 * recvfd - for the proc fd exchange.
 	 */
-	if (pledge("stdio cpath unix recvfd", NULL) == -1)
+	if (pledge("stdio cpath unix recvfd sendfd", NULL) == -1)
 		fatal("pledge");
 }
 
@@ -84,6 +84,8 @@ control_dispatch_vmd(int fd, struct privsep_proc *p, struct imsg *imsg)
 	switch (imsg->hdr.type) {
 	case IMSG_VMDOP_START_VM_RESPONSE:
 	case IMSG_VMDOP_PAUSE_VM_RESPONSE:
+	case IMSG_VMDOP_SEND_VM_RESPONSE:
+	case IMSG_VMDOP_RECEIVE_VM_RESPONSE:
 	case IMSG_VMDOP_UNPAUSE_VM_RESPONSE:
 	case IMSG_VMDOP_TERMINATE_VM_RESPONSE:
 	case IMSG_VMDOP_GET_INFO_VM_DATA:
@@ -96,7 +98,7 @@ control_dispatch_vmd(int fd, struct privsep_proc *p, struct imsg *imsg)
 			return (0);
 		}
 		imsg_compose_event(&c->iev, imsg->hdr.type,
-		    0, 0, -1, imsg->data, IMSG_DATA_SIZE(imsg));
+		    0, 0, imsg->fd, imsg->data, IMSG_DATA_SIZE(imsg));
 		break;
 	case IMSG_VMDOP_CONFIG:
 		config_getconfig(ps->ps_env, imsg);
@@ -368,13 +370,15 @@ control_dispatch_imsg(int fd, short event, void *arg)
 			log_setverbose(v);
 
 			/* FALLTHROUGH */
+		case IMSG_VMDOP_RECEIVE_VM_REQUEST:
+		case IMSG_VMDOP_SEND_VM_REQUEST:
 		case IMSG_VMDOP_PAUSE_VM:
 		case IMSG_VMDOP_UNPAUSE_VM:
 		case IMSG_VMDOP_LOAD:
 		case IMSG_VMDOP_RELOAD:
 		case IMSG_CTL_RESET:
 			if (proc_compose_imsg(ps, PROC_PARENT, -1,
-			    imsg.hdr.type, fd, -1,
+			    imsg.hdr.type, fd, imsg.fd,
 			    imsg.data, IMSG_DATA_SIZE(&imsg)) == -1)
 				goto fail;
 			break;
